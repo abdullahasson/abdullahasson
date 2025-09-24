de01f389ed24bfaa4be40c22318a8c70e12206d8
abdullahasson <bh532997@gmail.com>
2025-09-24 16:22:40 +0300

    update(1)

--- app/[locale]/_sections/Projects.tsx
+++ app/[locale]/_sections/Projects.tsx
@@ -1,8 +1,11 @@
+// Next
+import Image from "next/image"
 // Next Intl
 import { useTranslations } from "next-intl"
+// Motion
+import * as motion from "motion/react-client"
 // Components
 import Title from "@/components/Title"
-import ProjectCard from "@/components/Cards/ProjectCard"
 // images
 import HooBankImage from "@/images/HooBank.png"
 import BrainWaveImage from "@/images/brainwave.png"
@@ -48,7 +51,24 @@ const Projects = () => {
                 <div className="grid gap-12 grid-cols-2 max-[560px]:grid-cols-1">
                     {
                         projects.map((project, index) => (
-                            <ProjectCard data={project} key={index} />
+                            <motion.div
+                                className="group rounded-3xl p-5 relative overflow-hidden w-full h-[350px] cursor-pointer"
+                                whileHover={{ scale: 1.05 }} // Scale up on hover
+                                transition={{ type: 'spring', stiffness: 300 }} // Spring animation
+                                key={index}
+                            >
+                                <motion.div
+                                    className="absolute z-0 left-0 top-0 w-full h-full overflow-hidden"
+                                    whileHover={{ scale: 1.1, rotate: -2 }} // Zoom and rotate on hover
+                                    transition={{ duration: 0.5 }}
+                                >
+                                    <Image
+                                        src={project.img}
+                                        alt={`${project.title}-image`}
+                                        className="w-full h-full object-cover"
+                                    />
+                                </motion.div>
+                            </motion.div>
                         ))
                     }
                 </div>
